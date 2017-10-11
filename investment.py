d9aa12e66aa532d383b6ff2b4b7996a768832001
ZYM <zwdnet@163.com>
2017-10-12 07:23:28 +0800

    将模拟程序封装成类。

--- investment.py
+++ investment.py
@@ -23,63 +23,91 @@ print(len(df), len(df_etf), len(df_SH))
 import numpy as np
 
 print("沪深300指数与沪深300etf的相关性分析")
-# covariance = np.cov(df['close'], df_etf['close'])
-# print(covariance)
 corrc = np.corrcoef(df['close'], df_etf['close'])
 print(corrc)
 
 print("上证综指指数与沪深300etf的相关性分析")
-# covariance = np.cov(df_etf['close'][0:len(df_SH)], df_SH['close'])
-# print(covariance)
 corrc = np.corrcoef(df_etf['close'], df_SH['close'][len(df_SH)-len(df_etf):])
 print(corrc)
 
-# nask_data = df_nask['close'].values[::-1]
-# print(len(df['close']))
-# print(len(nask_data))
-# covariance2 = np.cov(df_etf['close'][0:732], nask_data)
-# print(covariance2)
-# corrc2 = np.corrcoef(df_etf['close'][0:732], nask_data)
-# print(corrc2)
+# 模拟的变量
+# n = len(df_SH['close'])
+# num = 1000.0  # 每次定投投入金额
+# fee = 3.0/10000.0 # 手续费率
+# total = [] # 每一期的总市值
+# number = 0 # 持股总数
+# stock = [] # 每一期购入的股票总数
+# invest = 0 # 总投入资金
+# rate = [] # 每期的收益率
+# cost = [] # 累计交易成本
+# costrate = [] # 每期的累计成本率
 
 # 定投模拟情况
 # 1.初始日期开始，每隔30天投一次，计算总盈利。
-n = len(df_SH['close'])
-num = 1000.0  # 每次定投投入金额
-fee = 3.0/10000.0 # 手续费率
-total = [] # 每一期的总市值
-number = 0 # 持股总数
-stock = [] # 每一期购入的股票总数
-invest = 0 # 总投入资金
-rate = [] # 每期的收益率
-cost = [] # 累计交易成本
-costrate = [] # 每期的累计成本率
 
-j = 0
-for i in range(0, n, 30):
-    stock.append(int((num/df_SH['close'].values[i])/100)*100)
-    number += stock[j]
-    investnow = stock[j] * df_SH['close'].values[i]   # 本期买的股票市值
-    costnow = investnow * fee
-    if costnow < 0.1:
-        costnow = 0.1
-    if j == 0:
-        cost.append(costnow)
-    else:
-        cost.append(cost[j-1] + costnow)
-    all = number * df_SH['close'].values[i] + (num - investnow)  - costnow  # 股票总市值与现金之和，扣除了成本。
-    total.append(all)
-    invest += num
-    rate.append(total[j]/invest)
-    costrate.append(cost[j]/float(invest))
-    print(i, total[j], rate[j], costrate[j]) # 输出每期投资后的总市值以及收益率
-    j = j+1
-p3 = pyplot.subplot(4,1,1)
-p4 = pyplot.subplot(4,1,2)
-p5 = pyplot.subplot(4,1,3)
-p6 = pyplot.subplot(4,1,4)
-p3.plot(total)
-p4.plot(rate)
-p5.plot(cost)
-p6.plot(costrate)
-pyplot.show()
+class Module(object):
+    def __init__(self, df_data):
+        self.n = len(df_data['close'])
+        self.df_data = df_data
+        self.num = 1000.0  # 每次定投投入金额
+        self.fee = 3.0/10000.0 # 手续费率
+        self.total = [] # 每一期的总市值
+        self.number = 0 # 持股总数
+        self.stock = [] # 每一期购入的股票总数
+        self.invest = 0 # 总投入资金
+        self.input = [] # 每期累计投入的总资金
+        self.rate = [] # 每期的收益率
+        self.cost = [] # 累计交易成本
+        self.costrate = [] # 每期的累计成本率
+
+    # 运行模拟
+    def run(self):
+        j = 0
+        for i in range(0, self.n, 30):
+            self.stock.append(int((self.num/self.df_data['close'].values[i])/100)*100)
+            self.number += self.stock[j]
+            self.investnow = self.stock[j] * self.df_data['close'].values[i]   # 本期买的股票市值
+            self.costnow = self.investnow * self.fee
+            if self.costnow < 0.1:
+                self.costnow = 0.1
+            if j == 0:
+                self.cost.append(self.costnow)
+            else:
+                self.cost.append(self.cost[j-1] + self.costnow)
+            all = self.number * self.df_data['close'].values[i] + \
+                (self.num - self.investnow)  - self.costnow  # 股票总市值与现金之和，扣除了成本。
+            self.total.append(all)
+            self.invest += self.num
+            self.input.append(self.invest)
+            self.rate.append(self.total[j]/self.invest)
+            self.costrate.append(self.cost[j]/float(self.invest))
+            # print(i, self.total[j], self.rate[j], self.costrate[j]) # 输出每期投资后的总市值以及收益率
+            j = j+1
+
+    # 作图
+    def draw(self):
+        # pyplot.rcParams['font.sans-serifSj'] = ['SimHei']
+        p1 = pyplot.subplot(4,1,1)
+        p2 = pyplot.subplot(4,1,2)
+        p3 = pyplot.subplot(4,1,3)
+        p4 = pyplot.subplot(4,1,4)
+        p1.plot(self.total)
+        p1.plot(self.input)
+        p1.set_title("All money of graph")
+        p2.plot(self.rate)
+        p2.set_title("All interst of graph")
+        p3.plot(self.cost)
+        p3.set_title("Cost graph")
+        p4.plot(self.costrate)
+        p4.set_title("Cost rate graph")
+        pyplot.show()
+
+    # 返回模拟结果
+    def getResult(self):
+        return self.total[-1], self.rate[-1], self.costrate[-1]
+
+module = Module(df_SH)
+module.run()
+module.draw()
+result = module.getResult()
+print(result)
